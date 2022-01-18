350fc9bccb34645f1e73df5e8873437caa637b47
omartuwaiq <91540868+omartuwaiq@users.noreply.github.com>
2022-01-18 19:16:19 +0300

    Add files via upload

---
+++ src/main/java/com/example/Serverside/ServersideApplication.java
@@ -0,0 +1,13 @@
+package com.example.Serverside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServersideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServersideApplication.class, args);
+	}
+
+}
